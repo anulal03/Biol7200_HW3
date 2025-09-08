9b84ca17773372e91250819c65fd0b79025b4ad6
Anu <anulal03@gmail.com>
2025-09-08 08:44:12 -0400

    updated code

--- find_homologs.sh
+++ find_homologs.sh
@@ -11,6 +11,5 @@ tblastn \
   -query "$query" \
   -subject "$subject" \
   -outfmt "6 qseqid sseqid pident length qlen" \
-  > "$output"
-awk '$3 > 30 && $4 > 0.9 * $5' > "$output"
+| awk '$3 > 30 && $4 > 0.9 * $5' > "$output"
 wc -l "$output"
