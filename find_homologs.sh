c2aa69ce8b21e12314a963b1c9091aca07bda14f
Anu <anulal03@gmail.com>
2025-09-07 23:35:23 -0400

    updated script

---
+++ find_homologs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#find_homologs.sh
+#find_homologs.sh <query> <subject file> <output file>
+
+query=$1
+subject=$2
+output=$3
+
+tblastn \
+  -query "$query" \
+  -subject "$subject" \
+  -outfmt "6 qseqid sseqid pident length qlen" \
+  > temp_results.txt
+awk '$3 > 30 && $4 > 0.9 * $5' temp_results.txt > filtered_results.txt
+wc -l filtered_results.txt
